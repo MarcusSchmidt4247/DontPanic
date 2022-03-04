d2e5f787ae2b959f5a9e799a02a668bcdaee1c6b
Marcus Schmidt <schmidt.marcust@gmail.com>
2022-03-04 14:19:23 -0800

    Fixed a couple of small issues

--- Android/app/src/main/java/com/example/dontpanic/GeneralUseActivity.java
+++ Android/app/src/main/java/com/example/dontpanic/GeneralUseActivity.java
@@ -1,6 +1,14 @@
 package com.example.dontpanic;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GeneralUseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_use);
+    }
 }
