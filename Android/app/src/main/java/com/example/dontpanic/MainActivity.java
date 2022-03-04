6f7f7dc3bb176bfbf878185f208a53e3447191bc
Tyrfing619 <scarlosnavy2@gmail.com>
2022-03-04 14:30:39 -0800

    small changes for onClick for Emergency/General Buttons.  Properly redirects.

--- Android/app/src/main/java/com/example/dontpanic/MainActivity.java
+++ Android/app/src/main/java/com/example/dontpanic/MainActivity.java
@@ -12,8 +12,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
     @Override
