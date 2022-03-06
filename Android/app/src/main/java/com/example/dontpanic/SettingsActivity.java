23b1b1bf2dffa927f3da498730762ad4236be113
Marcus Schmidt <schmidt.marcust@gmail.com>
2022-03-06 11:17:06 -0800

    First pass at Settings screen design

---
+++ Android/app/src/main/java/com/example/dontpanic/SettingsActivity.java
@@ -0,0 +1,18 @@
+// MS: 3/6/22 - initial code
+
+package com.example.dontpanic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
