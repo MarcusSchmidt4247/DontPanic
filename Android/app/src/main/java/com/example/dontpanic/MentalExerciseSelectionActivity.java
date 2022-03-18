03a42217af57ecd193922db875b63d19fe3af447
Diana Balderas <dbalderas1@csub.edu>
2022-03-18 13:06:01 -0700

    mental exercise selection screen

---
+++ Android/app/src/main/java/com/example/dontpanic/MentalExerciseSelectionActivity.java
@@ -0,0 +1,17 @@
+// DB: 3/18/22 - Created Java file for Mental Excercises Selection Screen
+
+package com.example.dontpanic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentalExerciseSelectionActivity extends AppCompatActivity {
+
+
+}
