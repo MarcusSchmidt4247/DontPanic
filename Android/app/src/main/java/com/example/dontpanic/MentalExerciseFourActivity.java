62a77ead5bff61238f67721f0fac86ee9ce85726
Diana Balderas <dbalderas1@csub.edu>
2022-03-18 18:36:50 -0700

    created third and fourth mental exercises screens

---
+++ Android/app/src/main/java/com/example/dontpanic/MentalExerciseFourActivity.java
@@ -0,0 +1,17 @@
+// DB: 3/18/22 - Created Java file for Mental Exercise Fourth Screen
+
+package com.example.dontpanic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentalExerciseFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentalexercise_four);
+
+    }
+}
---
+++ Android/app/src/main/java/com/example/dontpanic/MentalExerciseThreeActivity.java
@@ -0,0 +1,17 @@
+// DB: 3/18/22 - Created Java file for Mental Exercise Third Screen
+
+package com.example.dontpanic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentalExerciseThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentalexercise_three);
+
+    }
+}
