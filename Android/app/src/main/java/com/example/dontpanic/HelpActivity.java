db5fafc0cee3e5df7a5a156e786512fb480eb501
Marcus Schmidt <schmidt.marcust@gmail.com>
2022-03-20 09:16:19 -0700

    Fixed crashing issue

--- Android/app/src/main/java/com/example/dontpanic/HelpActivity.java
+++ Android/app/src/main/java/com/example/dontpanic/HelpActivity.java
@@ -1,5 +1,15 @@
 package com.example.dontpanic;
 
-public class HelpActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
 }
--- Android/app/src/main/java/com/example/dontpanic/NotificationsActivity.java
+++ Android/app/src/main/java/com/example/dontpanic/NotificationsActivity.java
@@ -1,5 +1,15 @@
 package com.example.dontpanic;
 
-public class NotificationsActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
 }
