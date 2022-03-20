245a1c3904fd25eeebc313e184bf3a3bce5369cc
Tyrfing619 <scarlosnavy2@gmail.com>
2022-03-20 13:42:56 -0700

    Some debugging regarding handling of the Module object, still WiP.  Check Logcat with search "Module" for detail.  Uncomment setText(s) to check nullObjectExceptions being thrown.

--- Android/app/src/main/java/com/example/dontpanic/Module.java
+++ Android/app/src/main/java/com/example/dontpanic/Module.java
@@ -76,4 +76,26 @@ public enum Module
 
         return "";
     }
+
+    public static Module InstanceOf(String _name)
+    {
+        for (Module module : Module.values())
+        {
+            if (module.name.equals(_name))
+                return module;
+        }
+
+        return null;
+    }
+
+    public static Module InstanceOf(int _id)
+    {
+        for (Module module : Module.values())
+        {
+            if (module.id == _id)
+                return module;
+        }
+
+        return null;
+    }
 }
--- Android/app/src/main/java/com/example/dontpanic/ModulesActivity.java
+++ Android/app/src/main/java/com/example/dontpanic/ModulesActivity.java
@@ -1,20 +1,43 @@
 package com.example.dontpanic;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ModulesActivity extends AppCompatActivity {
-    private static final Module[] modulesList = Module.values();
     private int id;
+
+    private TextView tvmName, tvmID, tvmClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        tvmName = findViewById(R.id.tvmName);
+        tvmID = findViewById(R.id.tvmName);
+        tvmClass = findViewById(R.id.tvmClass);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modules);
         this.id = 1;
-        ModuleReference tempModule = new ModuleReference(Module.GetName(this.id), this.id, Module.GetClass(this.id));
-        //set onClicker to listen to which button gets clicked (?)
-        System.out.println("Name: " + tempModule.getName() + "\nID: " + tempModule.getName() + "\nClass: " + tempModule.getType().toString());
+
+        Module module = Module.InstanceOf(id);
+        Log.i("Module created", module.GetClass(id).toString());
+        Log.i("Module Name", module.name);
+        Log.i("Module ID", String.format("%d", module.id));
+
+        //Bottom stuff below invokes crashing, as a NullPointer Exception keeps being thrown.  Will monitor this in a bit -SC
+
+        //tvmName.setText(module.name);
+        //tvmID.setText(String.format("d", module.id));
+
+        /*
+        if (module != null) {
+            tvmName.setText(String.format("Name: %s", module.name));
+            tvmID.setText(String.format("ID: %d", module.id));
+            tvmClass.setText(String.format("Class: %s", module.GetClass(module.id).toString()));
+        }
+         */
     }
 }
